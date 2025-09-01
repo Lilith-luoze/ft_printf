e223967fecb413d755db91ca67d74416bb6eebf2
laptop <xiaoshui@laptop.com>
2025-09-01 20:54:57 +0200

    reorg

--- ft_printf.c
+++ ft_printf.c
@@ -1,12 +1,7 @@
 #include "local_printf.h"
 #include "public_printf.h"
 
-int handle_format(const char **fmt_pp, va_list ap, t_fmt_parser *f_p)
-{
-    *fmt_pp = parse_fmt_main(*fmt_pp, f_p); // renewed the fmt pointer
-    parse_fmt_normalize(f_p);
-    return (dispatch_parsed(ap , *f_p));
-}
+int handle_format(const char **fmt_pp, va_list ap, t_fmt_parser *f_p);
 
 int ft_printf(const char *fmt, ...)
 {
@@ -35,6 +30,13 @@ int ft_printf(const char *fmt, ...)
     }
     va_end(ap);
     return count;
+}
+
+int handle_format(const char **fmt_pp, va_list ap, t_fmt_parser *f_p)
+{
+    *fmt_pp = parse_fmt_main(*fmt_pp, f_p); // renewed the fmt pointer
+    parse_fmt_normalize(f_p);
+    return (dispatch_parsed(ap , *f_p));
 }
 
 // # include <stdio.h>
