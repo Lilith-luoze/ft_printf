cf64d9f74ecf7866b131afc300b93ec951c314a8
laptop <xiaoshui@laptop.com>
2025-09-01 20:52:31 +0200

    reorg

---
+++ put_hex.c
@@ -0,0 +1,19 @@
+#include "local_printf.h"
+
+
+// prefix is with new things - 0x or 0X
+int put_hex(unsigned int h, t_fmt_parser f, int upper)
+{
+    t_numcfg n;
+    
+	n.negative = 0;
+	n.prefix_len = decide_prefix(n.negative, f, &n.prefix);
+	n.un = (unsigned long)h;
+	n.fil_len = utoa_hex_rev(n.un, n.buf, upper);
+	n.prec0 = num_prec0(f, &n.fil_len, n.un);
+	n.padding = f.width - (n.prefix_len + n.prec0 + n.fil_len);
+	if (n.padding < 0)
+		n.padding = 0;
+
+	return (put_num_cfg(n, f));
+}
---
+++ put_p.c
@@ -0,0 +1,19 @@
+#include "local_printf.h"
+
+
+int put_ptr(void *p, t_fmt_parser f)
+{
+    t_numcfg n;
+    
+	n.negative = 0;
+	n.prefix_len = 2;
+	n.prefix = "0x";
+	n.un = (unsigned long)p;
+	n.fil_len = utoa_hex_rev(n.un, n.buf, 0);
+	n.prec0 = num_prec0(f, &n.fil_len, n.un);
+	n.padding = f.width - (n.prefix_len + n.prec0 + n.fil_len);
+	if (n.padding < 0)
+		n.padding = 0;
+
+	return (put_num_cfg(n, f));
+}
