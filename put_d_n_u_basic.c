7833f18df0d3b18b777685966892a7c83801b9d1
laptop <xiaoshui@laptop.com>
2025-09-01 20:22:03 +0200

    ptr is done and temp moved

--- put_d_n_u_basic.c
+++ put_d_n_u_basic.c
@@ -37,17 +37,34 @@ int put_buffer_rev(char *buf, int len)
 	}
 	return (count);
 }
-int decide_prefix_num(int negative,t_fmt f, char * prefix)
+
+/// @brief decide the prefix for %duxXp
+/// @param negative 
+/// @param f 
+/// @param prefix_pp: pointer to pointer. modify the pointer to string
+/// @return the length of prefix
+int decide_prefix(int negative,t_fmt_parser f, char ** prefix_pp)
 {
+	if (f.hash && (f.spec == 'x' || f.spec == 'X'))
+	{
+		if (f.spec == 'x')
+			*prefix_pp = "0x";
+		else
+			*prefix_pp = "0X";
+		return (2);
+	}
 	if (negative)
-		*prefix = '-';
+		*prefix_pp = "-";
 	else if (f.plus)
-		*prefix = '+';
+		*prefix_pp = "+";
 	else if (f.space)
-		*prefix = ' ';
+		*prefix_pp = " ";
 	else
-		*prefix = '\0';
-	return (*prefix != '\0');
+	{
+		*prefix_pp = "";
+		return (0);
+	}
+	return (1);
 }
 
 int itoul(int n, unsigned long *un)
@@ -63,36 +80,36 @@ int itoul(int n, unsigned long *un)
 		return (0);
 	}
 }
-int num_prec0(t_fmt f, int *fil_len , unsigned long un)
+int num_prec0(t_fmt_parser f, int *fil_len , unsigned long un)
 {
-	int prec0s;
+	int prec0;
 
 	if (f.prec > *fil_len)
-		prec0s = f.prec - *fil_len;
+		prec0 = f.prec - *fil_len;
 	else if (f.prec == 0 && un == 0)
 	{
 		*fil_len = 0;
-		prec0s = 0;
+		prec0 = 0;
 	}
 	else
-		prec0s = 0;
-	return (prec0s);
+		prec0 = 0;
+	return (prec0);
 }
 
-int put_num_inctx(t_numctx n, t_fmt f)
+int put_num_cfg(t_numcfg n, t_fmt_parser f)
 {
 	int count;
 	count = 0;
 	if (f.minus)
 	{
-		count += write(1, n.prefix, n.has_prefix);
+		count += write(1, n.prefix, n.prefix_len);
 		count += put_repeat('0', n.prec0);
 		count += put_buffer_rev(n.buf, n.fil_len);
 		count += put_repeat(' ', n.padding);
 	}
 	else if (f.zero && f.prec < 0)
 	{
-		count += write(1, n.prefix, n.has_prefix);
+		count += write(1, n.prefix, n.prefix_len);
 		count += put_repeat('0', n.padding);
 		count += put_repeat('0', n.prec0);
 		count += put_buffer_rev(n.buf, n.fil_len);
@@ -100,7 +117,7 @@ int put_num_inctx(t_numctx n, t_fmt f)
 	else
 	{
 		count += put_repeat(' ', n.padding);
-		count += write(1, n.prefix, n.has_prefix);
+		count += write(1, n.prefix, n.prefix_len);
 		count += put_repeat('0', n.prec0);
 		count += put_buffer_rev(n.buf, n.fil_len);
 	}
@@ -110,34 +127,92 @@ int put_num_inctx(t_numctx n, t_fmt f)
 /// @brief Put decimal number to stdout, return number of characters written
 // handles sign, precision (leading zeros), width, and flags (+, space, 0, -).
 // prec never truncates digits
-int	put_d(int d, t_fmt f)
+int	put_d(int d, t_fmt_parser f)
 {
-	t_numctx n;
+	t_numcfg n;
 
 	n.negative = itoul(d, &n.un);
 	n.fil_len = utoa_dec_rev(n.un, n.buf);
 	n.prec0 = num_prec0(f, &n.fil_len, n.un);
-	n.has_prefix = decide_prefix_num(n.negative, f, &n.prefix);
-	n.padding = f.width - (n.has_prefix + n.prec0 + n.fil_len);
+	n.prefix_len = decide_prefix(n.negative, f, &(n.prefix));
+	n.padding = f.width - (n.prefix_len + n.prec0 + n.fil_len);
 	if (n.padding < 0)
 		n.padding = 0;
 
-	return (put_num_inctx(n, f));
+	return (put_num_cfg(n, f));
 }
 
-int put_u(unsigned int u, t_fmt f)
+int put_u(unsigned int u, t_fmt_parser f)
 {
-	t_numctx n;
+	t_numcfg n;
 
 	n.negative = 0;
+	n.prefix_len = 0;
 	n.un = (unsigned long)u;
 	n.fil_len = utoa_dec_rev(n.un, n.buf);
 	n.prec0 = num_prec0(f, &n.fil_len, n.un);
-	n.has_prefix = decide_prefix_num(n.negative, f, &n.prefix);
-	n.padding = f.width - (n.has_prefix + n.prec0 + n.fil_len);
+	n.padding = f.width - (n.prefix_len + n.prec0 + n.fil_len);
 	if (n.padding < 0)
 		n.padding = 0;
 
-	return (put_num_inctx(n, f));
+	return (put_num_cfg(n, f));
+}
+
+
+int utoa_hex_rev(unsigned long u, char *buf, int upper)
+{
+    int slen;
+    char *hex;
+    if (upper)
+        hex = "0123456789ABCDEF";
+    else 
+        hex = "0123456789abcdef";
+    slen = 0;
+    while(u >= 16)
+    {
+        *buf = hex[u % 16]; // a more standard way to index into a lookup string
+        buf ++;
+        slen++;
+        u /= 16;
+    }
+    *buf = hex[u % 16];
+    slen++;
+
+    return slen;
+}
+
+
+// prefix is with new things - 0x or 0X
+int put_hex(unsigned int h, t_fmt_parser f, int upper)
+{
+    t_numcfg n;
+    
+	n.negative = 0;
+	n.prefix_len = decide_prefix(n.negative, f, &n.prefix);
+	n.un = (unsigned long)h;
+	n.fil_len = utoa_hex_rev(n.un, n.buf, upper);
+	n.prec0 = num_prec0(f, &n.fil_len, n.un);
+	n.padding = f.width - (n.prefix_len + n.prec0 + n.fil_len);
+	if (n.padding < 0)
+		n.padding = 0;
+
+	return (put_num_cfg(n, f));
+}
+
+int put_ptr(void *p, t_fmt_parser f)
+{
+    t_numcfg n;
+    
+	n.negative = 0;
+	n.prefix_len = 2;
+	n.prefix = "0x";
+	n.un = (unsigned long)p;
+	n.fil_len = utoa_hex_rev(n.un, n.buf, 0);
+	n.prec0 = num_prec0(f, &n.fil_len, n.un);
+	n.padding = f.width - (n.prefix_len + n.prec0 + n.fil_len);
+	if (n.padding < 0)
+		n.padding = 0;
+
+	return (put_num_cfg(n, f));
 }
 
