fecaecd1292671febcd89bb9b564687d84f89c4e
laptop <xiaoshui@laptop.com>
2025-09-01 20:52:54 +0200

    content done?

---
+++ put_percent.c
@@ -0,0 +1,23 @@
+#include "local_printf.h"
+
+
+int	put_percent(t_fmt_parser f)
+{
+	int	count;
+
+	count = 0;
+	if (f.minus)
+	{
+		count += write(1, "%", 1);
+		count += put_repeat(' ', f.width - 1);
+	}
+	else
+	{
+		if (f.zero)
+			count += put_repeat('0', f.width - 1);
+		else
+			count += put_repeat(' ', f.width - 1);
+		count += write(1, "%", 1);
+	}
+	return (count);
+}
---
+++ put_s.c
@@ -0,0 +1,33 @@
+#include "local_printf.h"
+
+/// @brief  Put string to stdout, return number of characters written
+int	put_s(char *s , t_fmt_parser f)
+{
+	int	count;
+	const char *str;
+	size_t str_len;
+	if (!s)
+		return (write(1, &NULL_PTR,sizeof(NULL_PTR) - 1));
+	str = (const char *) s;
+	count = 0;
+	str_len = ft_strlen(str);
+
+	//prec
+	int eff_len;
+	if (f.prec >= 0 && f.prec < (int)str_len)
+		eff_len = f.prec;
+	else
+		eff_len =  (int)str_len;
+	//minus and width
+	if (f.minus)
+	{
+		count += write(1, str, eff_len);
+		count += put_repeat(' ', f.width - eff_len);
+	}
+	else
+	{
+		count += put_repeat(' ', f.width - eff_len);
+		count += write(1, str, eff_len);
+	}
+	return (count);
+}
