02c9b0de77f7f49305a11eca6989b54ae21fb59b
Luoze Guo <luozguo@2-H-2.42heilbronn.de>
2025-09-10 20:21:38 +0200

    modify write() to write_wrapper(), put_repeat() add an argument err_flag

--- put_s.c
+++ put_s.c
@@ -1,31 +1,44 @@
 #include "ft_printf.h"
 
+int put_s_effctive_length(char *s, t_fmt_parser f)
+{
+
+	int eff_len;
+	int str_len = 0;
+	str_len = (int)ft_strlen((const char *)s);
+	if (f.prec >= 0 && f.prec < str_len)
+		eff_len = f.prec;
+	else
+		eff_len =  str_len;
+	return (eff_len);
+}
+
 /// @brief  Put string to stdout, return number of characters written
 int	put_s(char *s , t_fmt_parser f)
 {
 	int	count;
-	size_t str_len;
+	int eff_len;
+	int err_flag;
+
 	if (s == NULL)
 		s = "(null)";
 	count = 0;
-	str_len = ft_strlen((const char *)s);
+	err_flag = 0;
 
-	//prec
-	int eff_len;
-	if (f.prec >= 0 && f.prec < (int)str_len)
-		eff_len = f.prec;
-	else
-		eff_len =  (int)str_len;
+	//create a function that deal with prec and str_len, obtain the effective length
+	eff_len = put_s_effctive_length(s, f);
 	//minus and width
 	if (f.minus)
 	{
-		count += write(1, s, eff_len);
-		count += put_repeat(' ', f.width - eff_len);
+		count += write_wrapper(1, s, eff_len, &err_flag);
+		count += put_repeat(' ', f.width - eff_len, &err_flag);
 	}
 	else
 	{
-		count += put_repeat(' ', f.width - eff_len);
-		count += write(1, s, eff_len);
+		count += put_repeat(' ', f.width - eff_len, &err_flag);
+		count += write_wrapper(1, s, eff_len, &err_flag);
 	}
+	if (err_flag)
+		return -1;
 	return (count);
 }
