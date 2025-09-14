e95692fd6c14063f17045e4c70a301bc4536127f
luozguo <anitadou9@gmail.com>
2025-09-14 19:35:25 +0200

    norm

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: luozguo <luozguo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/14 16:43:20 by luozguo           #+#    #+#             */
-/*   Updated: 2025/09/14 16:43:21 by luozguo          ###   ########.fr       */
+/*   Updated: 2025/09/14 19:31:47 by luozguo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,81 +14,87 @@
 # define FT_PRINTF_H
 
 # include <stdarg.h> // for variable argument lists (va_list, va_start, va_end)
+# include <stdlib.h> // for malloc and free()
 # include <unistd.h> // for write()
 
-typedef struct s_fmt {
-    int minus;    // '-' flag (0/1)
-    int zero;    // '0' flag (0/1)
-    
-    int width;   // -1 if not set
-    int prec;    // -1 if not set
+/// @brief most of members are set 0 by init. Only width, prec are -1 by init
+typedef struct s_fmt
+{
+	int		minus;
+	int		zero;
+	int		space;
+	int		hash;
+	int		plus;
+	int		if_ltr_percent;
+	char	spec;
 
-    char spec;   // c s p d i u x X %
+	int		width;
+	int		prec;
+}			t_fmt_parser;
 
-
-    int space;   // ' ' flag (0/1)
-    int hash;   // '#' flag (0/1) 
-    int plus;    // '+' flag (0/1)
-    int if_ltr_percent; // 1 if a literal % is needed to print and passed
-} t_fmt_parser;
-
+/// @brief
+///
 typedef struct s_num
 {
-	unsigned long	un;         // absolute value of the number
-	int				negative;   // 1 if original number < 0
-	char			buf[32];    // digits in reverse
-	int				fil_len;    // digit length
-	int				prec0;     // extra leading zeros from precision
-	char*			prefix;     //
-	int				prefix_len; 
-	int				padding;    // spaces/zeros for width
-}	t_numcfg;
+	unsigned long un;
+	int negative;     
+	char buf[32];     // digits in reverse
+	int fil_len;      // digit length
+	int prec0;        // extra leading zeros from precision
+	char *prefix;     //
+	int		prefix_len;
+	int padding; // spaces/zeros for width
+}			t_numcfg;
 
 // when spec p , arg is NULL, print (nil) in linux, 0x0 in mac
 # if defined(__linux__)
-# define NULL_PTR "(nil)"
+#  define NULL_PTR "(nil)"
 # elif defined(__APPLE__)
-# define NULL_PTR "0x0"
+#  define NULL_PTR "0x0"
 # endif
 
-
-void iterator_in_printf(va_list *ap, const char **fmt, t_fmt_parser *f, int *count);
-int		ft_printf(const char *fmt, ...);
+void		iterator_in_printf(va_list *ap, const char **fmt, t_fmt_parser *f,
+				int *count);
+int			ft_printf(const char *fmt, ...);
+int	handle_conversion(va_list *ap, const char **fmt, t_fmt_parser *f);
+int	advance_fmt_isnul(const char **fmt);
+void	literal_write_handler(const char **fmt, int *count, t_fmt_parser *f);
+int	write_with_copying_adding_0(int fd, int ori_len, const char *s);
+int	strlen_til_percent(const char *s);
 
 // put_helpers_1.c
-int put_repeat(char r, int len , int *err_flag);
-int utoa_dec_rev(unsigned int u, char *buf);
-int put_buffer_rev(char *buf, int len , int *err_flag);
-int decide_prefix_for_dx(int negative, t_fmt_parser f, char **prefix_pp);
-int write_wrapper(int fd, const void *buf, size_t len , int *err_flag);
+int			put_repeat(char r, int len, int *err_flag);
+int			utoa_dec_rev(unsigned int u, char *buf);
+int			put_buffer_rev(char *buf, int len, int *err_flag);
+int			decide_prefix_for_dx(int negative, t_fmt_parser f,
+				char **prefix_pp);
+int			write_wrapper(int fd, const void *buf, size_t len, int *err_flag);
 
 // put_helpers_2.c
-int itoul(int n, unsigned long *un);
-int num_prec0(t_fmt_parser f, int *fil_len, unsigned long un);
-int put_num_cfg(t_numcfg n, t_fmt_parser f);
-int utoa_hex_rev(unsigned long u, char *buf, int upper);
-size_t ft_strlen(const char *s);
+int			itoul(int n, unsigned long *un);
+int			num_prec0(t_fmt_parser f, int *fil_len, unsigned long un);
+int			put_num_cfg(t_numcfg n, t_fmt_parser f);
+int			utoa_hex_rev(unsigned long u, char *buf, int upper);
+size_t		ft_strlen(const char *s);
 
-//parser.c
-void parser_init(t_fmt_parser *f);
-int parse_number(const char **fmt);
-const char *parse_fmt(const char *fmt, t_fmt_parser *f_p);
-void parse_fmt_normalize(t_fmt_parser *f_p);
+// parser.c
+void		parser_init(t_fmt_parser *f);
+int			parse_number(const char **fmt);
+const char	*parse_fmt(const char *fmt, t_fmt_parser *f_p);
+void		parse_fmt_normalize(t_fmt_parser *f_p);
 
 // dispatcher.c
-int dispatch_parsed(va_list *ap, t_fmt_parser f);
+int			dispatch_parsed(va_list *ap, t_fmt_parser f);
 
 // put %c s d u x X p %
 
-int put_c(int c, t_fmt_parser f);
-int put_s(char *s, t_fmt_parser f);
-int put_d(int d, t_fmt_parser f);
-int put_u(unsigned int u, t_fmt_parser f);
-int put_hex(unsigned int u, t_fmt_parser f, int upper);
-int put_p(void *p, t_fmt_parser f);
-int put_percent(t_fmt_parser f);
-int put_num_cfg_left(t_numcfg n, int *err_flag);
-
-
+int			put_c(int c, t_fmt_parser f);
+int			put_s(char *s, t_fmt_parser f);
+int			put_d(int d, t_fmt_parser f);
+int			put_u(unsigned int u, t_fmt_parser f);
+int			put_hex(unsigned int u, t_fmt_parser f, int upper);
+int			put_p(void *p, t_fmt_parser f);
+int			put_percent(t_fmt_parser f);
+int			put_num_cfg_left(t_numcfg n, int *err_flag);
 
 #endif
