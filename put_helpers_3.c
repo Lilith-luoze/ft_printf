ee0f3482a54427306e4a111f345a60b7792e00f1
luozguo <anitadou9@gmail.com>
2025-09-14 19:35:36 +0200

    norm

--- put_helpers_3.c
+++ put_helpers_3.c
@@ -6,7 +6,7 @@
 /*   By: luozguo <luozguo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/14 19:12:33 by luozguo           #+#    #+#             */
-/*   Updated: 2025/09/14 19:14:23 by luozguo          ###   ########.fr       */
+/*   Updated: 2025/09/14 19:32:27 by luozguo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,4 +48,70 @@ int	parse_number(const char **fmt)
 		(*fmt)++;
 	}
 	return (result);
+}
+
+int	write_with_copying_adding_0(int fd, int ori_len, const char *s)
+{
+	int		ret;
+	int		i_str_with_0;
+	int		i_s;
+	char	*str_with_0;
+	int		first_dot;
+
+	first_dot = 1;
+	i_str_with_0 = 0;
+	i_s = 0;
+	str_with_0 = (char *)malloc(ori_len + 1);
+	if (!str_with_0)
+		return (-1);
+	while (i_s < ori_len)
+	{
+		str_with_0[i_str_with_0++] = s[i_s];
+		if (s[i_s] == '.' && first_dot)
+		{
+			str_with_0[i_str_with_0++] = '0';
+			first_dot = 0;
+		}
+		i_s++;
+	}
+	ret = write(fd, str_with_0, ori_len + 1);
+	free(str_with_0);
+	return (ret);
+}
+
+/// @brief first compute the number of non-percent literals and write them,
+///	and advance according fmt pointer for printf(); or if_ltr_percent is 1,
+///	go back one fmt and write ltr_fmt + 1 chars (extra literal % ), and advance
+/// fmt pointers. Always change count, printed_chars or on write error,
+///	set count to -1.
+/// @param fmt
+/// @param count
+/// @param f
+/// @return nothing
+void	literal_write_handler(const char **fmt, int *count, t_fmt_parser *f)
+{
+	int	fd;
+	int	ltr_len;
+	int	temp;
+
+	fd = 1;
+	ltr_len = strlen_til_percent(*fmt);
+	if (f->if_ltr_percent)
+	{
+		if (f->prec == 0 && f->spec == 0)
+			temp = write_with_copying_adding_0(fd, ltr_len + 1, (*fmt) - 1);
+		else
+			temp = write(fd, (*fmt) - 1, ltr_len + 1);
+		f->if_ltr_percent = 0;
+		(*fmt) += ltr_len;
+	}
+	else
+	{
+		temp = write(fd, *fmt, ltr_len);
+		(*fmt) += ltr_len;
+	}
+	if (temp == -1)
+		*count = -1;
+	else
+		*count += temp;
 }
